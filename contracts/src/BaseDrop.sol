b0b8623b9987a0ff3b71634c4ef1b816d1b310e1
Sendi John <johnsendi727@gmail.com>
2025-12-15 19:16:43 +0100

    feat: implement createCampaign

--- contracts/src/BaseDrop.sol
+++ contracts/src/BaseDrop.sol
@@ -8,18 +8,40 @@ contract BaseDrop is Ownable {
     uint256 public campaignCount;
 
     struct Campaign {
-    address creator;
-    IERC20 token;
-    uint256 rewardPerClaim;
-    uint256 referralBonus;
-    uint256 maxClaims;
-    uint256 claims;
-    bool active;
-}
+        address creator;
+        IERC20 token;
+        uint256 rewardPerClaim;
+        uint256 referralBonus;
+        uint256 maxClaims;
+        uint256 claims;
+        bool active;
+    }
 
- mapping(uint256 => Campaign) public campaigns;
- mapping(uint256 => mapping(address => bool)) public hasClaimed;
-
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => mapping(address => bool)) public hasClaimed;
 
     constructor() Ownable(msg.sender) {}
+
+    function createCampaign(
+        address token,
+        uint256 rewardPerClaim,
+        uint256 referralBonus,
+        uint256 maxClaims
+    ) external returns (uint256) {
+        require(maxClaims > 0, "invalid max");
+
+        campaignCount++;
+
+        campaigns[campaignCount] = Campaign({
+            creator: msg.sender,
+            token: IERC20(token),
+            rewardPerClaim: rewardPerClaim,
+            referralBonus: referralBonus,
+            maxClaims: maxClaims,
+            claims: 0,
+            active: true
+        });
+
+        return campaignCount;
+    }
 }
