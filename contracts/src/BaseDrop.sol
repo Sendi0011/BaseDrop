4a4f0e1c0820ee3ee882e63dfae5c417684dffd3
Sendi John <johnsendi727@gmail.com>
2025-12-15 19:15:17 +0100

    feat: add campaign struct and storage

--- contracts/src/BaseDrop.sol
+++ contracts/src/BaseDrop.sol
@@ -7,5 +7,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract BaseDrop is Ownable {
     uint256 public campaignCount;
 
+    struct Campaign {
+    address creator;
+    IERC20 token;
+    uint256 rewardPerClaim;
+    uint256 referralBonus;
+    uint256 maxClaims;
+    uint256 claims;
+    bool active;
+}
+
+ mapping(uint256 => Campaign) public campaigns;
+ mapping(uint256 => mapping(address => bool)) public hasClaimed;
+
+
     constructor() Ownable(msg.sender) {}
 }
